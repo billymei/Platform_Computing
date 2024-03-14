274466481ef8ca729f0c09a90570be724ab01c2f
billymei <146683760+billymei@users.noreply.github.com>
2024-03-13 21:55:21 -0700

    Add files via upload

---
+++ Assignment_2_User_Interactions/my-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>ABOUT ME</title>
+      <link rel="stylesheet" href="me.css" />
+      <h1>About Me</h1>
+      <p>Kono bangumi ha goran no sponsor de ookuri shimasu</p>
+      <div className="section">
+        hikikomori
+      </div>
+      <div className="section">
+        dame ningen
+      </div>
+      <div className="section">
+        anime otaku
+      <div className="subsection">
+        .-- .- - -.-. ....  -.-- .- -- .-  -. ---  ... ..- ... ..- -- .  
+      </div>
+    </div>
+    <div>
+      <img src="/images/02.jpg" alt="Yama no Susume" />
+    </div>
+</div>
+
+  );
+}
+
+export default App;
